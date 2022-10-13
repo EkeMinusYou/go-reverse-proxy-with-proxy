491bdffbe35063a68459fc68d61a3ab256c4b29c
EkeMinusYou <euonymuslke@gmail.com>
2022-10-13 19:45:47 +0900

    try normal server

--- main.go
+++ main.go
@@ -1,28 +1,34 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
-	"os"
 )
 
-var (
-	schema    = "http"
-	proxyHost = os.Getenv("PROXY_HOST")
-	port      = "8000"
-)
+// var (
+// 	schema    = "http"
+// 	proxyHost = os.Getenv("PROXY_HOST")
+// 	port      = "8000"
+// )
+
+// func main() {
+// 	// handler := &httputil.ReverseProxy{}
+// 	proxyUrl := &url.URL{
+// 		Scheme: schema,
+// 		Host:   proxyHost,
+// 	}
+// 	handler := httputil.NewSingleHostReverseProxy(proxyUrl)
+// 	log.Fatal(http.ListenAndServe(
+// 		":"+port,
+// 		handler,
+// 	))
+// }
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World")
+}
 
 func main() {
-	// handler := &httputil.ReverseProxy{}
-	proxyUrl := &url.URL{
-		Scheme: schema,
-		Host:   proxyHost,
-	}
-	handler := httputil.NewSingleHostReverseProxy(proxyUrl)
-	log.Fatal(http.ListenAndServe(
-		":"+port,
-		handler,
-	))
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
