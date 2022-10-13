ee6e12f9ac920e083a04362c0872df80b4525fdb
EkeMinusYou <euonymuslke@gmail.com>
2022-10-13 19:47:28 +0900

    restore proxy

--- main.go
+++ main.go
@@ -1,34 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
 )
 
-// var (
-// 	schema    = "http"
-// 	proxyHost = os.Getenv("PROXY_HOST")
-// 	port      = "8000"
-// )
-
-// func main() {
-// 	// handler := &httputil.ReverseProxy{}
-// 	proxyUrl := &url.URL{
-// 		Scheme: schema,
-// 		Host:   proxyHost,
-// 	}
-// 	handler := httputil.NewSingleHostReverseProxy(proxyUrl)
-// 	log.Fatal(http.ListenAndServe(
-// 		":"+port,
-// 		handler,
-// 	))
-// }
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World")
-}
+var (
+	schema    = "http"
+	proxyHost = os.Getenv("PROXY_HOST")
+	port      = "8000"
+)
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	// handler := &httputil.ReverseProxy{}
+	proxyUrl := &url.URL{
+		Scheme: schema,
+		Host:   proxyHost,
+	}
+	handler := httputil.NewSingleHostReverseProxy(proxyUrl)
+	log.Fatal(http.ListenAndServe(
+		":"+port,
+		handler,
+	))
 }
